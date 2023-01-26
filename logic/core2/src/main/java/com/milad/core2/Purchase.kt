823ebea290222c0174b39e84234b8e227f7c9d22
mohammad Arbabi <muhammad.arbabi@gmail.com>
2023-01-26 16:46:27 +0330

    change Purchase to abstract class and add attributes and methods

--- logic/core2/src/main/java/com/milad/core2/Purchase.kt
+++ logic/core2/src/main/java/com/milad/core2/Purchase.kt
@@ -1,6 +1,40 @@
 package com.milad.core2
 
-interface Purchase {}
+abstract class Purchase {
+    abstract val name : String
+    abstract val amount : Long
+    abstract val buyers : List<Person>
+    abstract val consumers : List<Person>
 
-class EqualPurchase() : Purchase {}
-class PercentagePurchase() : Purchase {}
+    abstract fun calculate() : Purchase
+}
+
+class EqualPurchase() : Purchase() {
+    override val name: String
+        get() = TODO("Not yet implemented")
+    override val amount: Long
+        get() = TODO("Not yet implemented")
+    override val buyers: List<Person>
+        get() = TODO("Not yet implemented")
+    override val consumers: List<Person>
+        get() = TODO("Not yet implemented")
+
+    override fun calculate(): Purchase {
+        TODO("Not yet implemented")
+    }
+}
+
+class PercentagePurchase() : Purchase() {
+    override val name: String
+        get() = TODO("Not yet implemented")
+    override val amount: Long
+        get() = TODO("Not yet implemented")
+    override val buyers: List<Person>
+        get() = TODO("Not yet implemented")
+    override val consumers: List<Person>
+        get() = TODO("Not yet implemented")
+
+    override fun calculate(): Purchase {
+        TODO("Not yet implemented")
+    }
+}
